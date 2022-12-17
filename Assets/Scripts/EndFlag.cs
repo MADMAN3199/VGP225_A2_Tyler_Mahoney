91d51cb15f5f7644985f7ad0fa137c30eb058b1e
MADMAN3199 <tylermahoney1999@hotmail.com>
2022-12-17 09:31:07 -0500

    Final

---
+++ Assets/Scripts/EndFlag.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndFlag : MonoBehaviour
+{
+    public string nextSceneName;
+    public bool lastLevel;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // did the player enter our trigger?
+        if(other.CompareTag("Player"))
+        {
+            // is this the last level?
+            if(lastLevel == true)
+            {
+                SceneManager.LoadScene(0);
+            }
+            // is there another scene/level we can go to?
+            else
+            {
+                SceneManager.LoadScene(nextSceneName);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+
+    // called when the player collects a coin
+    public void SetScoreText (int score)
+    {
+        scoreText.text = score.ToString();
+    }
+}
